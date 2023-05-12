166b41ba20401e0f0c66ea3cf90694ebc8f5a837
César Rosales <82127538+dwebcarc@users.noreply.github.com>
2023-05-12 18:21:53 -0300

    modifica consulta

--- desafio2_cesar_rosales_123.sql
+++ desafio2_cesar_rosales_123.sql
@@ -7,11 +7,11 @@ select sum(cantidad)
 from inscritos;
 
 --¿Cuál o cuáles son los registros de mayor antigüedad?
-select * from inscritos where fecha >= '2021-01-05';
+select * from inscritos where fecha = (select min(fecha) from inscritos);
 
 --¿Cuántos inscritos hay por día? (entendiendo un día como una fecha distinta de
 --ahora en adelante)
-select * from inscritos where fecha = (select min (fecha) from inscritos);
+select fecha,sum(cantidad) from inscritos group by fecha;
 
 --¿Qué día se inscribieron la mayor cantidad de personas y cuántas personas se inscribieron en ese día?
-select fecha,sum(cantidad) from inscritos group by fecha;
+select fecha,sum(cantidad) as total_inscritos from inscritos group by fecha order by total_inscritos desc limit 1;
